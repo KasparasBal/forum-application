fa6fcb64d4457f5a37d1f07831b912b56bba135c
Kasparas Balys <98013126+KasparasBal@users.noreply.github.com>
2022-08-01 13:17:52 +0300

    Commit 4: Added Readme.md & Started userController

---
+++ Server/controllers/userController.js
@@ -0,0 +1,7 @@
+const User = require("../models/userModel");
+const mongoose = require("mongoose");
+
+//Hashing
+const bcrypt = require("bcrypt");
+//Authentication
+const jwt = require("jsonwebtoken");
