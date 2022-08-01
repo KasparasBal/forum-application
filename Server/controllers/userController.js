4ccd555de1ac0d047b7f9928a15b2c7a3dad3900
Kasparas Balys <98013126+KasparasBal@users.noreply.github.com>
2022-08-01 14:49:53 +0300

    Commit 8: Added Login Endpoint

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt");
 //Authentication
 const jwt = require("jsonwebtoken");
 
-//Register EndPoint
+//Register
 
 const register = (req, res) => {
   //Hash the Password
@@ -46,6 +46,64 @@ const register = (req, res) => {
     });
 };
 
+//Login
+
+const login = (request, response) => {
+  // check if provided email exists
+  User.findOne({
+    email: request.body.email,
+  })
+
+    // if email exists
+    .then((user) => {
+      // compare  password  and  hashed password
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        .then((passwordCheck) => {
+          // passwords match?
+          if (!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords do not match",
+              error,
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            process.env.SECRET_KEY,
+            { expiresIn: "24h" }
+          );
+
+          // res success
+          response.status(200).send({
+            message: "Login Successful",
+            userId: user._id,
+            token,
+          });
+        })
+        // err - passwords don't match
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords do not match",
+            error,
+          });
+        });
+    })
+    // err -email not found
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
+        e,
+      });
+    });
+};
+
 module.exports = {
   register,
+  login,
 };
--- Server/routers/userRoutes.js
+++ Server/routers/userRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 //Controller
-const { register } = require("../controllers/userController");
+const { register, login } = require("../controllers/userController");
 
 //Register endpoint
-
 router.post("/register", register);
+
+//Login Endpoint
+router.post("/login", login);
 
 module.exports = router;
